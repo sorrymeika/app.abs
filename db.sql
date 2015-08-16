7def0cf97881333d6ad6685d7229cf38ac3668e5
sorrymeika <sorrymeika@163.com>
2015-08-17 03:10:25 +0800

    fx

--- db.sql
+++ db.sql
@@ -1,1 +1,27 @@
 select * from BMessage order by MSG_ID desc
+
+select * from BProspect
+select * from XProspect
+
+select * from BPurchase
+
+update BPurchase set PUR_PSP_ID= 59
+
+select * from RPayStatus
+
+select * from WProductPicture
+
+
+select * from BCashVoucher
+update BCashVoucher set CSV_PSP_ID= 59
+
+select * from RVoucherType
+
+select * from RPointOperationType
+
+select * from RPointType
+
+select * from HPoint
+update HPoint set HPT_PSP_ID= 59
+
+update XProspect set XPS_TOTAL_AMOUNT=1001,XPS_AVAILABLEPOINTS=100
---
+++ settings/data.js
@@ -0,0 +1,1 @@
+define("settings/data/adtypes.js",function(e,t,i){i.exports=[{value:"index",text:"未登录首页轮播图片"},{value:"index1",text:"已登录首页轮播图片"}]});

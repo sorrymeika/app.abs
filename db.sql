c0592594d493cc004da9235c8adec5e0363f8dd1
sorrymeika <sorrymeika@163.com>
2015-08-19 02:19:45 +0800

    fx

--- db.sql
+++ db.sql
@@ -4,10 +4,8 @@ select * from BProspect
 select * from XProspect
 
 select * from BPurchase
-
-update BPurchase set PUR_PSP_ID= 59
-
 select * from RPayStatus
+select * from RPurchaseStatus
 
 select * from WProductPicture
 
@@ -25,3 +23,34 @@ select * from HPoint
 update HPoint set HPT_PSP_ID= 59
 
 update XProspect set XPS_TOTAL_AMOUNT=1001,XPS_AVAILABLEPOINTS=100
+
+select * from BChannel
+select * From RChannelType
+
+select * from WFree join LFreCnl on LFC_FRE_ID=FRE_ID where FRE_FRT_ID=1
+select * from LFrePrd
+
+create table BPostMessage(
+MSG_ID int identity primary key,
+MSG_TITLE varchar(100),
+MSG_CONTENT varchar(500),
+MSG_DT datetime,
+MSG_RMT_ID int,
+MSG_CREATION_DT datetime,
+MSG_CREATIONUID varchar(50)
+)
+create table RMessageType (
+RMT_ID int primary key,
+RMT_DESC varchar(20)
+)
+insert into RMessageType values (1,'APP')
+
+create table LPspMsg (
+LPM_ID int identity primary key,
+LPM_MSG_ID int,
+LPM_PSP_ID int
+)
+
+select COUNT(1) from BPostMessage where not exists (select 1 from LPspMsg where LPM_PSP_ID=59 and MSG_ID=LPM_MSG_ID)
+
+select MSG_ID,MSG_TITLE,MSG_CONTENT,MSG_DT from BPostMessage left join LPspMsg on MSG_ID=LPM_MSG_ID where MSG_DT>GETDATE()

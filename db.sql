40493c288824f99800bc5d14b94e51d4cf9b5654
sorrymeika <sorrymeika@163.com>
2015-08-24 18:27:20 +0800

    fx

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ select * from WFree join LFreCnl on LFC_FRE_ID=FRE_ID
 select * from LPspFre
 
 update BPurchase set PUR_PSP_ID=59
-update XProspect set XPS_END_FREE_DT='2015-10-09',XPS_START_FREE_DT='2015-08-09' where XPS_PSP_ID=59
+update XProspect set XPS_END_FREE_DT='2015-10-09',XPS_START_FREE_DT='2015-05-09' where XPS_PSP_ID=59
 
 select XPS_START_FREE_DT,XPS_END_FREE_DT from XProspect where XPS_PSP_ID=59
 select Top 1 FRE_NAME,FRE_TITLE_PIC,FRE_PIC1,LPF_PUR_ID from WFree join LFreCnl on LFC_FRE_ID=FRE_ID left join (select LPF_PUR_ID,LPF_FRE_ID from LPspFre where LPF_PSP_ID=59) a on LPF_FRE_ID=FRE_ID where FRE_FRT_ID=1 and LFC_CNL_ID=1 and GETDATE() between LFC_START_TIME and LFC_END_TIME

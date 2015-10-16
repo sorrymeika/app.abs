d183d9cb760065a4db491be7dbd8a35fdb96ba3d
sorrymeika <sorrymeika@163.com>
2015-10-16 14:43:39 +0800

    fx

--- settings/data.js
+++ settings/data.js
@@ -1,1 +1,1 @@
-define("settings/data/adtypes.js",[],function(e,t,i){i.exports=[{value:"index",text:"未登录首页轮播图片"},{value:"index1",text:"已登录首页轮播图片"}]});
+define("settings/data/adtypes.js",[],function(e,t,i){i.exports=[{value:"launch",text:"启动页"},{value:"index1",text:"已登录首页轮播图片"},{value:"index",text:"未登录首页轮播图片"}]});
